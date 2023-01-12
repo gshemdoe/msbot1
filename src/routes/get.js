bed414e3021b112162d94c95269fdc4b686dad3d
gshemdoe <georgehmariki@gmail.com>
2023-01-12 16:23:20 +0300

    reply

--- src/routes/get.js
+++ src/routes/get.js
@@ -26,19 +26,4 @@ router.get('/webhook', async (req, res) => {
     }
 })
 
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
-
-}
-
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-
-}
-
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-
-}
-
 module.exports = router
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios').default
 
 router.post('/webhook', async (req, res) => {
     let body = req.body;
@@ -17,6 +18,13 @@ router.post('/webhook', async (req, res) => {
 
             let sender_psid = webhook_event.sender.id
             console.log('Sender ID is: '+ sender_psid)
+
+            //check if its message or postback
+            if(webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message)
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, )
+            }
         }
         // Returns a '200 OK' response to all requests
         res.status(200).send("EVENT_RECEIVED");
@@ -25,5 +33,60 @@ router.post('/webhook', async (req, res) => {
         res.sendStatus(404);
     }
 })
+
+
+
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+    let response;
+
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+      "recipient": {
+        "id": sender_psid
+      },
+      "message": response
+    }
+  
+    // Send the HTTP request to the Messenger Platform
+    axios.post('https://graph.facebook.com/v2.6/me/messages', {
+        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    }).then(()=> console.log('Message sent'))
+    .catch((err)=> console.log(err.message))
+
+    // request({
+    //   "uri": "https://graph.facebook.com/v2.6/me/messages",
+    //   "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    //   "method": "POST",
+    //   "json": request_body
+    // }, (err, res, body) => {
+    //   if (!err) {
+    //     console.log('message sent!')
+    //   } else {
+    //     console.error("Unable to send message:" + err);
+    //   }
+    // }); 
+  }
 
 module.exports = router
